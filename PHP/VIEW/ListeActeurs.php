1978ac19efdd5192ccf25ed4faf46ab132d72661
Rzied <galaxyzied@gmail.com>
2020-11-26 16:28:34 +0100

    liste

--- PHP/VIEW/ListeActeurs.php
+++ PHP/VIEW/ListeActeurs.php
@@ -3,14 +3,14 @@
 echo '<div class = "colonne">
 <h2>Liste des Acteurs</h2>
 <div class = "liste colonne">';
-echo '<a href ="index.php?codePage=formActeur&mode=ajout">Ajouter</a>';
+echo '<a class="ajout" href ="index.php?codePage=formActeur&mode=ajout">Ajouter</a>';
 $films = ActeursManager::getList();
 foreach ($films as $unFilm)
 {
-    echo '<div>'.$unFilm->getNomActeur()." ".$unFilm->getPrenomActeur().'</div>';
-    echo '<div><a href = "index.php?codePage=formActeur&mode=edit&id='.$unFilm->getIdActeur().'">Detail</a></div>';
-    echo '<div><a href = "index.php?codePage=formActeur&mode=modif&id='.$unFilm->getIdActeur().'">Modifier</a></div>';
-    echo '<div><a href = "index.php?codePage=formActeur&mode=delete&id='.$unFilm->getIdActeur().'">Supprimer</a></div>';
+    echo '<div class="text">'.$unFilm->getNomActeur()." ".$unFilm->getPrenomActeur().'</div>';
+    echo '<div><div><a class="detail btn" href = "index.php?codePage=formActeur&mode=edit&id='.$unFilm->getIdActeur().'">Detail</a></div>';
+    echo '<div><a class="modif btn" href = "index.php?codePage=formActeur&mode=modif&id='.$unFilm->getIdActeur().'">Modifier</a></div>';
+    echo '<div><a class="suppr btn" href = "index.php?codePage=formActeur&mode=delete&id='.$unFilm->getIdActeur().'">Supprimer</a></div></div>';
 
 }
 echo '</div>';
--- PHP/VIEW/ListeRealisateurs.php
+++ PHP/VIEW/ListeRealisateurs.php
@@ -3,14 +3,14 @@
 echo '<div class = "colonne">
 <h2>Liste des realisateurs</h2>
 <div class = "liste colonne">';
-echo '<a href ="index.php?codePage=formRealisateur&mode=ajout">Ajouter</a>';
+echo '<a class="ajout" href ="index.php?codePage=formRealisateur&mode=ajout">Ajouter</a>';
 $realisateurs = RealisateursManager::getList();
 foreach ($realisateurs as $realisateur)
 {
-    echo '<div>'.$realisateur->getNomRealisateur()." ".$realisateur->getPrenomRealisateur().'</div>';
-    echo '<div><a href = "index.php?codePage=formRealisateur&mode=edit&id='.$realisateur->getIdRealisateur().'">Detail</a></div>';
-    echo '<div><a href = "index.php?codePage=formRealisateur&mode=modif&id='.$realisateur->getIdRealisateur().'">Modifier</a></div>';
-    echo '<div><a href = "index.php?codePage=formRealisateur&mode=delete&id='.$realisateur->getIdRealisateur().'">Supprimer</a></div>';
+    echo '<div class="text">'.$realisateur->getNomRealisateur()." ".$realisateur->getPrenomRealisateur().'</div>';
+    echo '<div><div><a class="detail btn" href = "index.php?codePage=formRealisateur&mode=edit&id='.$realisateur->getIdRealisateur().'">Detail</a></div>';
+    echo '<div><a class="modif btn" href = "index.php?codePage=formRealisateur&mode=modif&id='.$realisateur->getIdRealisateur().'">Modifier</a></div>';
+    echo '<div><a class="suppr btn" href = "index.php?codePage=formRealisateur&mode=delete&id='.$realisateur->getIdRealisateur().'">Supprimer</a></div></div>';
 
 }
 echo '</div>';
