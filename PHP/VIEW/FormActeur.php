15f69ba3eb200d0ce6cc1ae85b8c3b0e503e5d2c
unknown <tancraftwork@gmail.com>
2020-11-26 11:40:46 +0100

    formActeur

--- PHP/VIEW/FormActeur.php
+++ PHP/VIEW/FormActeur.php
@@ -4,11 +4,7 @@ $mode = $_GET['mode'];
 switch ($mode){
 case "ajout" :
     {
-<<<<<<< HEAD
         echo '<form action="index.php?CodePagePage=actionActeur&mode=ajoutActeur" method="POST">';
-=======
-        echo '<form action="index.php?codePage=actionActeur&mode=ajoutActeur" method="POST">';
->>>>>>> 06708fde12bfcadeb6f238562d9e095dbe58bd9d
         break;
     }
 case "edit" :
@@ -18,20 +14,12 @@ case "edit" :
     }
 case "modif" :
     {
-<<<<<<< HEAD
         echo '<form action="index.php?CodePage=actionActeur&mode=modifActeur" method="POST">';
-=======
-        echo '<form action="index.php?codePage=actionActeur&mode=modifActeur" method="POST">';
->>>>>>> 06708fde12bfcadeb6f238562d9e095dbe58bd9d
     break;
     }
 case "delete" :
     {
-<<<<<<< HEAD
         echo '<form action="index.php?CodePage=actionActeur&mode=delActeur" method="POST">';
-=======
-        echo '<form action="index.php?codePage=actionActeur&mode=delActeur" method="POST">';
->>>>>>> 06708fde12bfcadeb6f238562d9e095dbe58bd9d
     break;
     }
 
