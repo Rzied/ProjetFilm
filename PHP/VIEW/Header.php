2a28f6af44ef971a85d64bd20a9ce66acb2c6ee6
Quentin2706 <quentin.balair2706@gmail.com>
2020-11-26 15:03:21 +0100

    ajout bouton accueil

--- PHP/VIEW/Header.php
+++ PHP/VIEW/Header.php
@@ -1,8 +1,12 @@
 
 <header>
+    <a href="index.php?codePage=default">
         <div><img class="logo" src="./IMG/logo.png" alt="logo"></div>
+    </a>
         <div class="titre colonne centrer"><div class="centrer">Mediatech !</div><div class="soustitre centrer">Le site par excellence pour retrouver les meilleurs films du moment !</div></div>
+    <a href="index.php?codePage=default">
         <div><img class="logo" src="./IMG/logo.png" alt="logo"></div>
+    </a>
     </header>
     <nav>
         <div></div>
--- PHP/VIEW/accueil.php
+++ PHP/VIEW/accueil.php
@@ -3,7 +3,6 @@ echo '
     <div class="bcgblack">
     <div></div>
     <div class="block accueilfond grosflex">
-        
         <img class="grosflex" src="./IMG/main.jpg" alt="image de fond">
     </div>
     <div></div>
