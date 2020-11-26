d16a21b89f4f573ae81c904f1b97383aa2f68443
Quentin2706 <quentin.balair2706@gmail.com>
2020-11-26 14:08:33 +0100

    Accueil patch boutons

--- PHP/VIEW/accueil.php
+++ PHP/VIEW/accueil.php
@@ -3,27 +3,27 @@ echo '
     <nav>
         <div></div>
         <div>
-        <a href="index.php?codePage=ListeFilms">
+        <a href="index.php?codePage=listeFilms">
             <div class="boutons">
                 Liste des Films
             </div>
         </a>
-        <a href="index.php?codePage=ListeActeurs">
+        <a href="index.php?codePage=listeActeurs">
             <div class="boutons">
                 Liste des Acteurs
             </div>
         </a>
-        <a href="index.php?codePage=ListeGenres">
+        <a href="index.php?codePage=listeGenres">
             <div class="boutons">
                 Listes des Genres
             </div>
         </a>
-        <a href="index.php?codePage=ListeRealisateurs">
+        <a href="index.php?codePage=listeRealisateurs">
             <div class="boutons">
                 Listes des Réalisateurs
             </div>
         </a>
-        <a href="index.php?codePage=ListeStudios">
+        <a href="index.php?codePage=listeStudios">
             <div class="boutons">
                 Listes des Studios
             </div>
