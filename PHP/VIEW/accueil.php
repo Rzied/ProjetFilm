613a7ff8b7cd47d562632fbc7ca2b2c6e6939f63
Quentin2706 <quentin.balair2706@gmail.com>
2020-11-26 12:18:44 +0100

    accueil

---
+++ PHP/VIEW/accueil.php
@@ -0,0 +1,32 @@
+<?php
+echo '    
+    <header>
+        <div><img class="logo" src="logo.png" alt="logo"></div>
+        <div class="titre colonne centrer"><div class="centrer">Mediatech !</div><div class="soustitre centrer">Le site par excellence pour retrouver les meilleurs films du moment !</div></div>
+        <div><img class="logo" src="logo.png" alt="logo"></div>
+    </header>
+    <nav>
+        <div></div>
+        <div>
+            <div class="boutons">
+                Liste des Films
+            </div>
+            <div class="boutons">
+                Liste des Acteurs
+            </div>
+            <div class="boutons">
+                Listes des Studios
+            </div>
+        </div>
+        <div></div>
+    </nav>
+    <div class="bcgblack">
+    <div></div>
+    <div class="block accueilfond grosflex">
+        
+        <img class="grosflex" src="main.jpg" alt="">
+    </div>
+    <div></div>
+</div>
+</body>
+</html>';
